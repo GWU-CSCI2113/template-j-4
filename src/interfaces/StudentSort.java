bd40a401b82d236303968569b3c11e6402561a13
Tim Wood <timwood@gwu.edu>
2016-11-01 22:16:04 -0400

    delete solutions since they break things.

--- src/interfaces/StudentSort.java
+++ src/interfaces/StudentSort.java
@@ -1,25 +1,24 @@
 package interfaces;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class StudentSort {
     public static void main(String[] args) {
 
-    	List<Student> names = new ArrayList<Student>();
-        
-        names.add(new Student("Joan", "Smith", 3));
-        names.add(new Student("Karen", "Chen", 4));
-        names.add(new Student("Jeff", "Smith", 3));
-        names.add(new Student("Tom", "Richy", 3));
-        names.add(new Student("Jack", "Li", 2));
-        names.add(new Student("Luke", "Macabe", 2));
-        
+    	List<StudentSolved> names = new ArrayList<StudentSolved>();
+
+        names.add(new StudentSolved("Joan", "Smith", 3));
+        names.add(new StudentSolved("Karen", "Chen", 4));
+        names.add(new StudentSolved("Jeff", "Smith", 3));
+        names.add(new StudentSolved("Tom", "Richy", 3));
+        names.add(new StudentSolved("Jack", "Li", 2));
+        names.add(new StudentSolved("Luke", "Macabe", 2));
+
         System.out.println("Unsorted names:");
         System.out.println(names);
-        
-        // Collections.sort(names);		// Uncomment this to sort! 
+
+        // Collections.sort(names);		// Uncomment this to sort!
         System.out.println("\n\nSorted names:");
         System.out.println(names);
     }
