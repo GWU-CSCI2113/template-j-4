6bb33dd3211d69ebb1bd77256986d8b1545533da
Tim Wood <timwood@gwu.edu>
2016-11-01 22:19:36 -0400

    fix bug

--- src/interfaces/StudentSort.java
+++ src/interfaces/StudentSort.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class StudentSort {
     public static void main(String[] args) {
 
-    	List<StudentSolved> names = new ArrayList<StudentSolved>();
+    	List<Student> names = new ArrayList<Student>();
 
-        names.add(new StudentSolved("Joan", "Smith", 3));
-        names.add(new StudentSolved("Karen", "Chen", 4));
-        names.add(new StudentSolved("Jeff", "Smith", 3));
-        names.add(new StudentSolved("Tom", "Richy", 3));
-        names.add(new StudentSolved("Jack", "Li", 2));
-        names.add(new StudentSolved("Luke", "Macabe", 2));
+        names.add(new Student("Joan", "Smith", 3));
+        names.add(new Student("Karen", "Chen", 4));
+        names.add(new Student("Jeff", "Smith", 3));
+        names.add(new Student("Tom", "Richy", 3));
+        names.add(new Student("Jack", "Li", 2));
+        names.add(new Student("Luke", "Macabe", 2));
 
         System.out.println("Unsorted names:");
         System.out.println(names);
