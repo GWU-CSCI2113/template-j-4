d3ab08ec66c71ef9169f0b6e700ef1058d1ebe3d
Tim Wood <timwood@gwu.edu>
2018-10-30 15:34:14 -0400

    Read a file example.

---
+++ src/files/RandReader.java
@@ -0,0 +1,33 @@
+package files;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class RandReader {
+
+	public static void main(String args[]) {
+		try {
+			BufferedReader in
+				= new BufferedReader(new FileReader("romeo.txt"));
+
+			String line = in.readLine();
+			int i=1;
+
+			while(line != null) {
+				System.out.println(line);
+				line = in.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Error! File not found!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error reading from file!");
+			e.printStackTrace();
+		}
+
+	}
+}
